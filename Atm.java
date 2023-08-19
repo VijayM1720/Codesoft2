e4e6c40ba6a3127aa4eeb30fb14260e322b60adb
Vijay Mahajan <142301254+VijayM1720@users.noreply.github.com>
2023-08-19 15:38:57 +0530

    Add files via upload
    Certainly! The code provided is structured into three main parts:
    
    BankAccount Class: This class represents the bank account of the user. It has functions to check the account balance, deposit money into the account, and withdraw money from the account.
    
    ATM Class: The ATM class manages the interactions with the user. It displays a menu of options like checking the balance, depositing money, and withdrawing money. It takes the user's input, processes it, and interacts with the BankAccount class to perform the requested actions.
    
    Main Class: The Main class is where the program begins. It creates an instance of the Bank Account class with an initial balance and then sets up the ATM using that account. The ATM's "run" method is called to start the interaction with the user.

---
+++ Atm.java
@@ -0,0 +1,53 @@
+package Com.ATM;
+
+import java.util.Scanner;
+
+class ATM {
+    private BankAccount userAccount;
+
+    public ATM(BankAccount account) {
+        userAccount = account;
+    }
+
+    public void displayMenu() {
+        System.out.println("Welcome to the ATM!");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");  
+        System.out.println("4. Exit");
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            displayMenu();
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Your balance: $" + userAccount.getBalance());
+                    break;
+                case 2:
+                    System.out.print("Enter deposit amount: $");
+                    double depositAmount = scanner.nextDouble();
+                    userAccount.deposit(depositAmount);
+                    break;
+                case 3:
+                    System.out.print("Enter withdrawal amount: $");
+                    double withdrawalAmount = scanner.nextDouble();
+                    userAccount.withdraw(withdrawalAmount);
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+
+        } while (choice != 4);
+    }
+}
